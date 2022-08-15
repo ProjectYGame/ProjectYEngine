bf4dd624ac752591ea9e130e437a5202be2fba52
Aleksey <bulldog.seregin@gmail.com>
2022-08-15 15:18:45 +0300

    Logger config

--- project-client/src/main/java/net/projecty/client/ProjectYClient.java
+++ project-client/src/main/java/net/projecty/client/ProjectYClient.java
@@ -7,13 +7,19 @@ import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.util.Animator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ProjectYClient implements GLEventListener {
+
+	private static final Logger logger = LogManager.getLogger(ProjectYClient.class);
 	public static void main(String[] args) {
 		new ProjectYClient();
 	}
 
 	private ProjectYClient() {
+		logger.debug("ProjectYClient::construct() - START");
+
 		GLCapabilities caps = new GLCapabilities(GLProfile.get(GLProfile.GL2GL3));
 		GLWindow glWindow = GLWindow.create(caps);
 
@@ -32,6 +38,8 @@ public class ProjectYClient implements GLEventListener {
 		Animator animator = new Animator();
 		animator.add(glWindow);
 		animator.start();
+
+		logger.debug("ProjectYClient::construct() - END");
 	}
 
 	@Override
@@ -45,6 +53,7 @@ public class ProjectYClient implements GLEventListener {
 
 	@Override
 	public void dispose(GLAutoDrawable drawable) {
+		logger.debug("ProjectYClient::dispose() - STOP APP");
 		System.exit(0);
 	}
 }
